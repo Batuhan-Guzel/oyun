d3b084621798a2e21fc51f22c4da40c460156bdf
Batuhan Güzel <batuhanguzel35@gmail.com>
2025-05-14 18:42:34 +0300

    bullet sınıfı eklendi

---
+++ Bullet.py
@@ -0,0 +1,18 @@
+from main import screen_width, screen_height, white  
+import pygame
+
+
+class Bullet(pygame.sprite.Sprite):
+    def _init_(self, x, y):
+        super()._init_()
+        self.image = pygame.Surface((5, 10))  
+        self.image.fill(white)  
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)  
+        self.speed = 7  
+
+    def update(self):
+        self.rect.y -= self.speed  
+        if self.rect.bottom < 0:  
+            self.kill()
+            #..
